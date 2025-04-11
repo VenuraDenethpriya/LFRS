b37ddc996b6f3ee2370cb1654e6481e8e0c8e01f
VenuraDenethpriya <157047923+VenuraDenethpriya@users.noreply.github.com>
2025-04-11 13:45:10 +0530

    Update api.js

--- Frontend/src/lib/api.js
+++ Frontend/src/lib/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // Define a service using a base URL and expected endpoints
 export const reportApi = createApi({
   reducerPath: "reportApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000/api/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://lfrs.onrender.com/api/" }),
   /*prepareHeaders: async (headers, { getState}) => {
     const token = await window.__clerk_domain.session.getToken();
  
